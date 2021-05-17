380558d6fcece3c0ccdb73ad8d35ec430974e32e
Luis Estrada <lestradaeu@gmail.com>
2021-05-17 17:27:06 -0600

    Actualizacion con archivos finales

--- Desarrollo_Proyecto/js/controlador_amortizacion.js
+++ Desarrollo_Proyecto/js/controlador_amortizacion.js
@@ -189,3 +189,38 @@ function limpiar() {
   }
 
 
+function graficos(fechas,cuota, amortizacionConstante, pagoInteres, monto){
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['cuota','amortizacionConstante','pagoInteres','monto'],
+            datasets: [{
+                label: '$ruta',
+                data: [$cuota,$amortizacionConstante,$pagoInteres,$monto],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+    
+
+}
